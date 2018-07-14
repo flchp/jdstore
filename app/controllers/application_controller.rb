da5d76c32df698ee4ea77486f8705975691fd77a
chenhuaping <18770688010@139.com>
2018-07-14 15:24:30 +0800

    bug fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   helper_method :current_cart
+  def admin_required
+    if !current_user.admin?
+      redirect_to "/", alert: "You are not admin."
+    end
+  end
 
   def current_cart
     @current_cart ||= find_cart
